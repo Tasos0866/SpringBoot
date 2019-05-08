6d0aa511b6417f17da0394e606e8bd06cbafc65b
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-05-08 20:18:27 +0300

    Created a basic api with fake data containing students and their info.

---
+++ src/main/java/com/tasos/Entity/Student.java
@@ -0,0 +1,39 @@
+package com.tasos.Entity;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String course;
+
+    public Student(int id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+    public Student() {}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
---
+++ src/main/java/com/tasos/Service/StudentService.java
@@ -0,0 +1,34 @@
+package com.tasos.Service;
+
+import com.tasos.Dao.StudentDao;
+import com.tasos.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents() {
+        return studentDao.getAllStudents();
+    }
+    public Student getStudentById(int id) {
+        return this.studentDao.getStudentById(id);
+    }
+
+    public void removeStudentById(int id) {
+        this.studentDao.removeStudentById(id);
+    }
+
+    public void updateStudent(Student student) {
+        this.studentDao.updateStudent(student);
+    }
+
+    public void insertStudent(Student student) {
+        this.studentDao.insertStudentToDb(student);
+    }
+}
